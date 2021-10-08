a56ed30e3c1e93714f271652cf78e1ee10bd58c2
RodResq <rodrigoresque@yahoo.com.br>
2021-10-08 00:53:24 -0300

    first commit

--- usuario.py
+++ usuario.py
@@ -1,5 +1,6 @@
 class Usuario:
     def __init__(self, nome, idade, sobrenome):
+        """Cria um objecto q representa um usuario"""
         self.nome = nome
         self.idade = idade
         self.sobrenome = sobrenome
