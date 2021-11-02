dcf2539e1d1d41aba73230b3b249cbc87e67468e
RodResq <rodrigoresque@yahoo.com.br>
2021-11-02 11:53:03 -0300

    Boas praticas python, dir e help

--- dir_help.py
+++ dir_help.py
@@ -2,9 +2,13 @@
 
 Ultilitarios Pyton que facilitam na programacao
 
-Dir -> Apresenta todos os atributos/propriedades e funcoes/metodos disponiveis para determinado tipo de dado ou varivel.
+dir -> Apresenta todos os atributos/propriedades e funcoes/metodos disponiveis para determinado tipo de dado ou varivel.
 
-Help -> Apresenta a documentacao/como ultilizar os atributos/propriedades e funcoes/metodos disponiveis para determinado
+dir(tipo de dado/variavel)
+
+help -> Apresenta a documentacao/como ultilizar os atributos/propriedades e funcoes/metodos disponiveis para determinado
 tipo de dado ou variavel.
+
+help(tipo de dados/variavel.propriedade/methods/funcoes)
 
 """
