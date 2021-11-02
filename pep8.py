9d5d3b2ad79a8e6a7776bea787ada4ecea6a5edf
RodResq <rodrigoresque@yahoo.com.br>
2021-11-02 11:22:08 -0300

    Boas praticas python, pep8

---
+++ pep8.py
@@ -0,0 +1,29 @@
+"""
+
+[5] - Imports
+
+- Imports devem ser sempre feitos em linhas separadas
+
+# Import Errado:
+
+import sys, os
+
+# Import Certo
+
+import sys
+import os
+
+# Nao ha problemas em ultilizar
+
+from types import StringType, ListType
+
+# Caso tenha muitos imports de um mesmo pacote, recomenda-se fazer
+
+from types import (
+    StryngType,
+    ListType,
+    SetType,
+    OutroType
+)
+
+"""
