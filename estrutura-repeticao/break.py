6783e8e1bd3214411ed43927aca3fd8a834661a2
RodResq <rodrigoresque@yahoo.com.br>
2021-11-14 22:32:20 -0300

    python updt

---
+++ estrutura-repeticao/break.py
@@ -0,0 +1,12 @@
+
+for num in range(1, 11):
+    if num == 6:
+        break
+    else:
+        print(num)
+print('Sai do loop')
+
+while True:
+    comando = input('Digite sair pra sair: ')
+    if comando == 'sair':
+        break
---
+++ estrutura-repeticao/loop_while.py
@@ -0,0 +1,22 @@
+"""
+
+Loop While
+
+while expressao_booleana:
+    //execucao do loop
+
+O bloco do while sera repetido enquanto a expressao booleana sera verdadeira.
+
+numero = 1
+
+while numero < 10:
+    print(numero)
+    numero = numero + 1
+
+"""
+
+resposta = ''
+
+while resposta != 'sim':
+    resposta = input('Ja acabou ? ')
+print('Saiu do Loop While')
