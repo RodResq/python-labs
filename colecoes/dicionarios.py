1c4352873d61032816056ab44038b250ee1c6b1c
RodResq <rodrigoresque@yahoo.com.br>
2021-11-27 12:14:39 -0300

    dicionarios review - 6

--- colecoes/dicionarios.py
+++ colecoes/dicionarios.py
@@ -109,8 +109,6 @@ print(receita)
 del receita['fev']
 print(receita)
 
-"""
-
 # Ulizando Lista
 carrinho = []
 
@@ -123,4 +121,72 @@ carrinho.append(carrinho2)
 print(carrinho)
 
 # Ultilizando Tuplas
+
+produto1 = ('Playstation', 1, 2300.00)
+produto2 = ('God of War 4', 1, 150.00)
+
+carrinho = (produto1, produto2)
+
+print(carrinho)
+
+# Ultilizando Dicionario
+
+carrinho = []
+
+carrinho1 = {'nome': 'Playstation 4', 'quantidade': 1, 'valor': 2300.00}
+carrinho2 = {'nome': 'God of War', 'quantodade': 1, 'valor': 150.00}
+
+carrinho.append(carrinho1)
+carrinho.append(carrinho2)
+print(type(carrinho))
+print(carrinho)
+
+
+print(d)
+print(type(d))
+
+d.clear()
+
+print(d)
+
+# Deep Coopy
+novo = d.copy();
+
+# novo.update({'d': 4})
+novo['d'] = 4
+
+print(novo)
+
+d = dict(a=1, b=2, c=3)
+
+# Shalow copy
+novo = d
+
+novo['d'] = 4
+
+print(novo)
+print(d)
+
+
+"""
+
+# Forma nao usual de criar dictionary
+
+outro = {}.fromkeys('a', 'b')
+
+print(type(outro))
+print(outro)
+
+# Outra forma
+
+outro = {}.fromkeys(['nome', 'pontos', 'email', 'profile'], 'desconhecido')
+print(type(outro))
+print(outro)
+
+veja = {}.fromkeys('teste', 'valor')
+print(veja)
+
+veja = {}.fromkeys(range(1, 11), 'valor')
+print(veja)
+
 
