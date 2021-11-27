512280c8c26fd1e8cf16686d54de8f6210eb5b32
RodResq <rodrigoresque@yahoo.com.br>
2021-11-27 11:35:08 -0300

    dicionarios review - 3

--- colecoes/dicionarios.py
+++ colecoes/dicionarios.py
@@ -24,10 +24,6 @@ paises = dict(br='Braisl', eua='Estados Unidos', py='Paraguai')
 print(paises)
 print(type(paises))
 
-"""
-
-paises = {'br': 'Brasil', 'eua': 'Estados Unidos', 'py': 'Paraguai'}
-
 # Acessando Valores
 # Forma 1
 print(paises['br'])
@@ -39,3 +35,71 @@ print(paises['br'])
 # Acessando via get
 print(paises.get('br'))
 print(paises.get('ru'))
+
+if pais:
+    print(f'Encontrei o pais {pais}')
+else:
+    print('Nao encontrei o pais')
+
+Podemos definir uma valor padrao, caso nao encontremos o objeto
+pais = paises.get('ru', 'Nao encontrado')
+print(f'Encontrei o pais {pais}')
+
+# Podemos verficar se determinda chave in dicionario
+
+paises = {'br': 'Brasil', 'eua': 'Estados Unidos', 'py': 'Paraguai'}
+
+print('br' in paises)
+print('ru' in paises)
+print('Estados Unidos' in paises)
+
+if 'ru' in paises:
+    russia = paises['ru']
+
+localidades = {
+    (35.6895, 396917): 'Escritorio em Toqui',
+    (40.7128, 74.0060): 'Escritorio em Nova York',
+    (37.7749, 122.4194): 'Escritorio em Sao Paulo'
+}
+
+print(localidades)
+print(type(localidades))
+
+
+# Adcionar Elementos em um dicionario
+
+receita = {'jan': 100, 'fev': 120, 'mar': 300}
+
+print(receita)
+print(type(receita))
+
+# Forma 1
+
+receita['abril'] = 350
+print(receita)
+
+# Forma 2 - Mais Elegante
+
+novo_dado = {'mai': 500}
+receita.update(novo_dado) # Outra forma: receita.update({'mai', 500})
+
+print(receita)
+
+# Atualizando Dados em um dicionario:
+
+receita['mai'] = 550
+print(receita)
+
+receita.update({'mai': 600})
+print(receita)
+
+"""
+
+
+# Remover dados de um dicionario
+
+receita = {'jan': 100, 'fev': 120, 'mar': 300}
+
+receita.pop('mar')
+
+
---
+++ colecoes/tipo_none.py
@@ -0,0 +1,5 @@
+
+numeros = None
+
+print(numeros)
+print(type(numeros))
