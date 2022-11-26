498a2bc84e6964fb6595686b2323dfe7571e3c56
RodResq <rodrigoresque@yahoo.com.br>
2022-11-26 17:29:35 -0300

    @Reduce

---
+++ funcoes-interadas/reduce.py
@@ -0,0 +1,14 @@
+"""
+Reduce
+obs: A partir do python 3+ nao é uma função builtin, ultiliza-lá apartir do módulo functools
+
+Guido van Rossum: Ultilize a função reduce() se relamnete ultilzar ela, na maioria dos casas ultilizar o loop for
+"""
+from functools import reduce
+
+dados = [2, 3, 4, 5, 7, 11, 13, 17, 19, 23, 29]
+
+mult = lambda x, y: x * y
+
+res = reduce(mult, dados)
+print(res)
