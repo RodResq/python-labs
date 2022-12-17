fd509d8cffdcfe8eb1c2b0122f573f9daa23943d
RodResq <rodrigoresque@yahoo.com.br>
2022-12-17 15:18:08 -0300

    updt

---
+++ funcoes-interadas/min_e_max.py
@@ -0,0 +1,19 @@
+"""
+Min e Max
+
+max() => retorna o valor de um interavel ou o maior valor de dois ou mais elementos
+
+
+"""
+lista = [1, 8, 4, 99, 34, 129]
+print(max(lista))
+
+tupla = (1, 8, 4, 99, 34, 129)
+print(max(tupla))
+
+conjunto = {1, 8, 4, 99, 34, 129}
+print(max(conjunto))
+
+dicionario = {'a': 1, 'b': 8, 'c': 4, 'd': 99, 'e': 34, 'f': 129}
+print(max(dicionario.values()))
+
