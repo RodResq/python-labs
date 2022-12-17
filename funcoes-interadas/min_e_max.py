507e777329f10cde93e43dcd35ab97fe6130d875
RodResq <rodrigoresque@yahoo.com.br>
2022-12-17 15:33:12 -0300

    updt

--- funcoes-interadas/min_e_max.py
+++ funcoes-interadas/min_e_max.py
@@ -1,10 +1,8 @@
 """
-Min e Max
+min e max
 
 max() => retorna o valor de um interavel ou o maior valor de dois ou mais elementos
 
-
-"""
 lista = [1, 8, 4, 99, 34, 129]
 print(max(lista))
 
@@ -16,4 +14,67 @@ print(max(conjunto))
 
 dicionario = {'a': 1, 'b': 8, 'c': 4, 'd': 99, 'e': 34, 'f': 129}
 print(max(dicionario.values()))
+
+print(max(3, 34))
+
+val1 = int(input('Informe o valor 1: '))
+val2 = int(input('Informe o valor 2: '))
+
+print(max(val1, val2))
+
+print(max('a', 'ab', 'abc'))
+
+print(max('a', 'b', 'c'))
+
+print(max(3.45345, 5.65776))
+
+print(max('Geek University'))
+
+
+#################################
+
+min() => Retorna o menor valor em um interavel ou o menor de dois ou mais elementos
+
+lista = [1, 8, 4, 99, 34, 129]
+print(min(lista))
+
+tupla = (1, 8, 4, 99, 34, 129)
+print(min(tupla))
+
+conjunto = {1, 8, 4, 99, 34, 129}
+print(min(conjunto))
+
+dicionario = {'a': 1, 'b': 8, 'c': 4, 'd': 99, 'e': 34, 'f': 129}
+print(min(dicionario.values()))
+
+print(min(3, 34))
+
+val1 = int(input('Informe o valor 1: '))
+val2 = int(input('Informe o valor 2: '))
+
+print(min(val1, val2))
+
+print(min('a', 'ab', 'abc'))
+
+print(min('a', 'b', 'c'))
+
+print(min(3.45345, 5.65776))
+
+print(min('Geek University'))
+
+"""
+
+
+# Outros Exemplos
+
+nomes = ['Arya', 'Samson', 'Dora', 'Tim', 'Olivander']
+print(max(nomes))   # Tim
+print(min(nomes))   # Arya
+
+print(max(nomes, key=lambda nome: len(nome)))  # Olivander
+print(min(nomes, key=lambda nome: len(nome)))  # Tim
+
+
+
+
 
