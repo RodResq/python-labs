df7c480694155ae6764d5d710e3ea20bd1d0d659
RodResq <rodrigoresque@yahoo.com.br>
2023-01-07 15:54:24 -0300

    updt

---
+++ debuggando-tratando-erros/debugando_com_pdb.py
@@ -0,0 +1,15 @@
+
+# comando basico do pdb:
+# l (listar onde estamos no codigo)
+# n (proxima linha)
+# p (imprimir variavel)l
+
+# c (Continua a execucao) - finaliza o debugiing
+
+
+nome = 'Angelina'
+sobrenome = 'Jolie'
+breakpoint()
+nome_completo = nome + ' ' + sobrenome
+curso = 'Programacao em python: essential'
+final = nome_completo + ' faz o curso ' + curso
