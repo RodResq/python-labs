e9d5e205328a1547c74f99be86cd7fbaa76939d2
RodResq <rodrigoresque@yahoo.com.br>
2023-02-05 17:42:13 -0300

    updt

--- trabalhando_com_modulos/modulos_builtin.py
+++ trabalhando_com_modulos/modulos_builtin.py
@@ -1,7 +1,69 @@
 """
   Trabalhando com Modulos Builtin(Modulos integrados no Python)
 
-__________________________
-| Python | Mdulos Builtin
---------------------------
+______________________
+|Python|Mdulos Builtin
+----------------------
+# ultilizando alias (apelidos) para mdoulos/funcoes
+
+import random as rdm
+
+print(rdm.random())
+
+#Podemos importar todas as funcoes de um modulo ultilizando o *
+
+from random import *
+
+print(random())
+
+
+#Importndo todo o modulo
+
+import random
+print(random.random())
+
+# Ultilizando alias para a funcao
+from random import randint as rdi
+
+print(rdi(5, 89))
+
+# Ultilizando alias para a funcao
+from random import randint as rdi, random as rdm
+
+print(rdi(5, 89))
+print(rdm())
+
+
 """
+
+
+#Costumamos ultilizar tuple para colocar multiplos import so mesmo modulo
+# from random import random, randint, shuffle, sample, choice
+
+from random import (
+    random, 
+    randint, 
+    shuffle,
+    choice,
+    sample
+)
+
+print(random())
+print(randint(1, 54))
+lista = ['Geek', 'University', 'Python']
+shuffle(lista)
+print(lista)
+
+print(choice(lista))
+
+
+
+
+
+
+
+
+
+
+
+
