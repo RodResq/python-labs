150c12f343047c2ab584a73e2a5dcc8d1164cfdf
RodResq <rodrigoresque@yahoo.com.br>
2023-02-11 17:19:38 -0300

    updt

--- trabalhando_com_modulos/pacotes.py
+++ trabalhando_com_modulos/pacotes.py
@@ -1,5 +1,12 @@
 """
 Pacotes 
 
+Modulo -> É apenas um arquivo python que pode ter diversas funcoes para ultilizarmos;
+
+Pacote -> É um diretório contendo uma coleção de módulos;
+
+OBS: Nas versões  2.x do python pu menores deveria conter um arquivo dentro dos diretrios chamado: __init__py.
+
+Nas versaoes do Python 3.x nao é obrigatorio a ultlizacao deste arquivo, mas ainda é ultlizado pra manter compatibilidade.
 
 """
